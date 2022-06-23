d591b8c8b83d7483473c9018d09ec85f6fabbed5
Cierra Sparks <csparks04@hotmail.com>
2022-06-23 13:26:00 -0400

    Set Fruit Name Column as the Index

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # Import Data
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Create Menu
